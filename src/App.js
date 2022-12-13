2732d95fed12f7f3b3f0e7c4e935e9a21885d9c1
Aman9392 <amanpawar8412@gmail.com>
2022-12-13 13:42:41 +0530

    Edite navabr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/General" element={<News key="general" pageSize={6} country="in" category="general" />}></Route>
+            <Route exact path="/Business" element={<News key="Business" pageSize={6} country="in" category="Business" />}></Route>
+            <Route exact path="/Entertainment" element={<News key="Entertainment" pageSize={6} country="in" category="Entertainment" />}></Route>
+            <Route exact path="/Science" element={<News key="Science" pageSize={6} country="in" category="Science" />}></Route>
+            <Route exact path="/Technology" element={<News key="Technology" pageSize={6} country="in" category="Technology" />}></Route>
+            <Route exact path="/Sports" element={<News key="Sports" pageSize={6} country="in" category="Sports" />}></Route>
+            <Route exact path="/Health" element={<News key="Health" pageSize={6} country="in" category="Health" />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
